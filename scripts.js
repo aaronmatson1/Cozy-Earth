7e3cc3bc17b98871288ba9ed072aacbef370c723
matsonaaron1 <matsonaaron1@gmail.com>
2022-10-24 22:36:40 -0600

    minor code fixes

--- scripts.js
+++ scripts.js
@@ -8,13 +8,16 @@ $(function(){
 
 function myFunction() {
   var x = document.getElementById("myDIV");
+  var title = document.getElementById("title");
 
   if (x.style.display === "none") {
     console.log('show card')
     x.style.display = "block";
+    title.style.display = "none";
   } else {
     console.log('hide card');
     x.style.display = "none";
+    title.style.display = "block"
     
   }
 }
